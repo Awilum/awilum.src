b7934ce8851d7ea0cc842abe52cb63950bd9a067
Sergey Romanenko <awilum@msn.com>
2023-09-25 21:33:26 +0800

    add prism

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    // Can be 'shiki' (default), 'prism' or false to disable highlighting
+    syntaxHighlight: 'prism',
+  },
   integrations: [tailwind()]
 });
