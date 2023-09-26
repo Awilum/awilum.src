7580eda150cb5eb6812a633ba90af6e7365bdcc0
Sergey Romanenko <awilum@msn.com>
2023-09-26 16:35:53 +0800

    update site title and site description consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'AWILUM';
+export const SITE_DESCRIPTION = 'Digital Nomad. Software Engineer. E-Learning Instructor.';
 export const categories = {
     "php": {
         "title": "PHP",
