16a746f1debe1fa400f9da3fd2905cbc4f4ea300
Sergey Romanenko <awilum@msn.com>
2023-10-01 16:18:18 +0800

    next round of project refactoring

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
+import alpinejs from "@astrojs/alpinejs";
+
 // https://astro.build/config
 export default defineConfig({
   site: "http://localhost:4321",
   markdown: {
     // Can be 'shiki' (default), 'prism' or false to disable highlighting
-    syntaxHighlight: "prism",
+    syntaxHighlight: "prism"
   },
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    sitemap(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), sitemap(), alpinejs()]
 });
