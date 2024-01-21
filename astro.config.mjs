17b946f5ddb77019f830cc1ebfa11b181c7a80b3
Sergey Romanenko <awilum@msn.com>
2024-01-21 13:40:03 +0700

    add pagefind search

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,21 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
   outDir: "./dist",
   site: "https://awilum.ru",
   compressHTML: true,
+  build: {
+    format: "directory",
+  },
   markdown: {
     // Can be 'shiki' (default), 'prism' or false to disable highlighting
     syntaxHighlight: "prism"
   },
   integrations: [tailwind({
     applyBaseStyles: false
-  }), sitemap()]
+  }), sitemap(), pagefind()]
 });
